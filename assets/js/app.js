0dea6dfb95b87a9f2ff63770a4ba5349a1be7293
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-11-28 19:10:50 -0200

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,8 @@ define([
 	'angular',
 	'angularRoute',
 	'controllers/controllers',
-	'services/services',
 	'constants/constants',
 	'directives/directives'
 	], function(angular){
-		return angular.module('app', ['ngRoute', 'controllers', 'services', 'constants', 'directives'])
+		return angular.module('app', ['ngRoute', 'controllers', 'constants', 'directives'])
 	});
